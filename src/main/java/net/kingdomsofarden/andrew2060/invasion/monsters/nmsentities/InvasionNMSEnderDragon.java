ecea1563c22d4d449960bfd41e20d75976163933
Andrew2060 <2060andrew@gmail.com>
2013-08-23 13:33:31 -0700

    Implement Boss Dragons/Withers with skill use

---
+++ src/main/java/net/kingdomsofarden/andrew2060/invasion/monsters/nmsentities/InvasionNMSEnderDragon.java
@@ -0,0 +1,12 @@
+package net.kingdomsofarden.andrew2060.invasion.monsters.nmsentities;
+
+import net.minecraft.server.v1_6_R2.EntityEnderDragon;
+import net.minecraft.server.v1_6_R2.World;
+
+public class InvasionNMSEnderDragon extends EntityEnderDragon {
+
+    public InvasionNMSEnderDragon(World world) {
+        super(world);
+    }
+
+}
---
+++ src/main/java/net/kingdomsofarden/andrew2060/invasion/monsters/nmsentities/InvasionNMSWitherBoss.java
@@ -0,0 +1,12 @@
+package net.kingdomsofarden.andrew2060.invasion.monsters.nmsentities;
+
+import net.minecraft.server.v1_6_R2.EntityWither;
+import net.minecraft.server.v1_6_R2.World;
+
+public class InvasionNMSWitherBoss extends EntityWither {
+
+    public InvasionNMSWitherBoss(World world) {
+        super(world);
+    }
+
+}
