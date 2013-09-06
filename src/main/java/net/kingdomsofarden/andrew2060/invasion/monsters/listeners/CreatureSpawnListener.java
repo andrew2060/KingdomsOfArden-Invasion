98552e10b70bdaec194742c90f8c2f4e081f9ce6
Andrew2060 <2060andrew@gmail.com>
2013-09-06 01:15:16 -0700

    Scale mob health/damage/exp based on distance from 0,0

--- src/main/java/net/kingdomsofarden/andrew2060/invasion/monsters/listeners/CreatureSpawnListener.java
+++ src/main/java/net/kingdomsofarden/andrew2060/invasion/monsters/listeners/CreatureSpawnListener.java
@@ -7,6 +7,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Giant;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,11 +17,16 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.characters.Monster;
+
 
 public class CreatureSpawnListener implements Listener {
     private MobManager mobmanager;
+    private Heroes heroes;
     public CreatureSpawnListener(MobManager mobmanager) {
         this.mobmanager = mobmanager;
+        this.heroes = (Heroes)Bukkit.getPluginManager().getPlugin("Heroes");
     }
     @EventHandler(ignoreCancelled = true)
     public void spawnGiant(PlayerInteractEvent event) {
@@ -38,10 +45,26 @@ public class CreatureSpawnListener implements Listener {
     }
     
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onCreatureSpawn(CreatureSpawnEvent event) {
+    public void onCreatureSpawnRegisterCharacter(CreatureSpawnEvent event) {
         if(event.getEntity() instanceof Creature) {
             mobmanager.registerCharacter((Creature) event.getEntity());
         }
+    }
+    
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onCreatureSpawnModifyHealthDamage(CreatureSpawnEvent event) {
+        if(event.getEntity() instanceof HumanEntity) {
+            return;
+        }
+        Location spawnLoc = event.getLocation();
+        double distanceSquared = event.getLocation().getWorld().getSpawnLocation().distanceSquared(spawnLoc);
+        double multiplier = Math.pow(1.5,(distanceSquared/100000000));
+        LivingEntity lE = event.getEntity();
+        lE.setMaxHealth(lE.getMaxHealth()*multiplier);
+        lE.setHealth(lE.getMaxHealth());
+        Monster m = heroes.getCharacterManager().getMonster(lE);
+        m.setDamage(0.5*multiplier > 1 ? m.getDamage()*0.5*multiplier : m.getDamage());
+        m.setExperience((int) Math.floor(m.getExperience()*multiplier));
     }
 }
 
